6c336e4e146d862d2be07d980ca57ea3ec050dff
pk305 <pknuek@gmail.com>
2026-02-02 11:54:14 +0300

    update route

--- config/sidebar.php
+++ config/sidebar.php
@@ -30,7 +30,7 @@ return [
                     'title' => 'Approvals',
                     'route' => 'approvals.index',
                     'icon' => 'bx bx-check-circle',
-                    'permission' => 'app.dashboard.approvals'
+                    'permission' => 'app.dashboard.view'
                 ],
                 [
                     'title' => 'Mail',
